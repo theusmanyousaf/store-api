3e7642def8f9110803a72a039dd6e2359dba8258
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-17 16:47:32 +0500

    sorting products result

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 const ProductsModel = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
-    const products = await ProductsModel.find({})
+    const products = await ProductsModel.find({}).sort("-name")
     res.status(200).json({ products, nbHits: products.length})
 }
 
 const getAllProducts = async (req,res) => {
-    const {featured, company, name} = req.query
+    const {featured, company, name, sort} = req.query
     const queryObject = {}
 
     if(featured){
@@ -19,7 +19,14 @@ const getAllProducts = async (req,res) => {
         queryObject.name = {$regex: name, $options: 'i'}
     }
     
-    const products = await ProductsModel.find(queryObject)
+    let result = ProductsModel.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt') // default sorting for result
+    }
+    const products = await result;
     res.status(200).json({products, nbHits: products.length})
 }
 
